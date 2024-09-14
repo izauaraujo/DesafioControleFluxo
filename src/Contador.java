819754a430fb49c5db247cc3ae66813fcad0e54c
izauaraujo <izauaraujo@gmail.com>
2024-09-13 22:34:21 -0300

    Desafio finalizado

--- src/Contador.java
+++ src/Contador.java
@@ -20,7 +20,7 @@ public class Contador {
     }
     static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-        if (parametroUm > parametroDois) {
+        if (parametroUm >= parametroDois) {
             throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
 
         }else{
